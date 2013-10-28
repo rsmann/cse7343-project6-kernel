a733546a15b0db0cfbba11b54b53b4dc71c24fb7
Scott Mann <ssmann@smu.edu>
2013-10-28 17:22:21 -0500

    Adding some debugging code so I can tell when my code stops executing...

--- kernel.c
+++ kernel.c
@@ -7,6 +7,7 @@
 void printString(int ln, int col, int color, char* message);
 void printHelloWorld();
 void clearScreen();
+void printEnd();
 
 int columnCount = 80;
 int rowCount = 25;
@@ -19,37 +20,60 @@ int videoMemoryBase = 0xB8000;
 
 int main()
 {
-	char* message = "Test";
-
 	clearScreen();
 	printHelloWorld();
-	printString(5, 5, 0x7, message);
+	/*printString(5, 5, 0x7, "Testing...");*/
+
+	printEnd();
+
+	while (1==1) {}
 }
 
+void printEnd()
+{
+	putInMemory(segmentBase, memoryBase + 3840, 'E');
+	putInMemory(segmentBase, memoryBase + 3841, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 3842, 'n');
+	putInMemory(segmentBase, memoryBase + 3843, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 3844, 'd');
+	putInMemory(segmentBase, memoryBase + 3845, colorWhite);
+
+}
 void printString(int ln, int col, int color, char* message)
 {
 	int relativeAddress = 0;
 	int len = 0;
 	int i = 0;
-	char* message2 = "Hello again!";
 
-	relativeAddress = ((columnCount * (ln-1) * 2) + (col*2));
-	len = sizeof(message2)/sizeof(char);
+	relativeAddress = ((columnCount * (ln-1)) * 2); /* + (col*2));*/
+	relativeAddress = 160;
+	/*len = sizeof(message2)/sizeof(char);*/
 	len = 12;
+
+	putInMemory(segmentBase, relativeAddress, 'B');
+	putInMemory(segmentBase, relativeAddress + 2, 'o');
+	putInMemory(segmentBase, relativeAddress + 4, 'o');
+
+	/*
 
 	while (i < len)
 	{
-		putInMemory(segmentBase, relativeAddress + i, message2[i/2]);
+		putInMemory(segmentBase, relativeAddress + i, *message2[i/2]);
 		putInMemory(segmentBase, relativeAddress + i + 1, color);
 
 		i = i + 2;
 	}
+*/
 
 	return;
 }
 
 void printHelloWorld()
 {
+	int j = 0;
+
 	putInMemory(segmentBase, memoryBase, 'H');
 	putInMemory(segmentBase, memoryBase + 1, colorWhite);
 
@@ -85,6 +109,14 @@ void printHelloWorld()
 
 	putInMemory(segmentBase, memoryBase + 22, '!');
 	putInMemory(segmentBase, memoryBase + 23, colorWhite);
+
+	/* Delay before returning to allow the screen to catch up */
+	/*
+	for (j=0; j++; j<1000)
+	{
+
+	}
+	*/
 }
 
 void clearScreen()
@@ -94,14 +126,14 @@ void clearScreen()
 
 	while (i < screenCharCount*2)
 	{
-		putInMemory(segmentBase, memoryBase + i, ' ');
+		putInMemory(segmentBase, memoryBase + i, '\0');
 		putInMemory(segmentBase, memoryBase + i + 1, colorWhite);
 
 		i=i+2;
 	}
 
 	/* Delay before returning to allow the screen to catch up */
-	for (j=0; j++; j<1000)
+	for (j=0; j++; j<100)
 	{
 
 	}
