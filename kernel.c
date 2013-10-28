cac58821fabdae7161ec9eab623c4df8d17ab390
Scott Mann <ssmann@smu.edu>
2013-10-28 12:15:03 -0500

    Testing with a hard coded version first.  Memory offsets in my loop are wrong somewhere.

--- kernel.c
+++ kernel.c
@@ -20,6 +20,7 @@ int videoMemoryBase = 0xB8000;
 int main()
 {
 	clearScreen();
+	printHelloWorld();
 }
 
 /*
@@ -29,18 +30,62 @@ void printString(int ln, int col, char* message)
 }
 */
 
-void clearScreen()
+void printHelloWorld()
 {
-	int i = 0;
-
-	for (i=memoryBase; i+=2; i<screenCharCount)
-	{
-		putInMemory(segmentBase, i, ' ');
-		putInMemory(segmentBase, i+1, colorWhite);
-	}
-
-	putInMemory(segmentBase, memoryBase, 'A');
+	putInMemory(segmentBase, memoryBase, 'H');
 	putInMemory(segmentBase, memoryBase + 1, colorWhite);
 
+	putInMemory(segmentBase, memoryBase + 2, 'e');
+	putInMemory(segmentBase, memoryBase + 3, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 4, 'l');
+	putInMemory(segmentBase, memoryBase + 5, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 6, 'l');
+	putInMemory(segmentBase, memoryBase + 7, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 8, 'o');
+	putInMemory(segmentBase, memoryBase + 9, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 10, ' ');
+	putInMemory(segmentBase, memoryBase + 11, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 12, 'W');
+	putInMemory(segmentBase, memoryBase + 13, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 14, 'o');
+	putInMemory(segmentBase, memoryBase + 15, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 16, 'r');
+	putInMemory(segmentBase, memoryBase + 17, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 18, 'l');
+	putInMemory(segmentBase, memoryBase + 19, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 20, 'd');
+	putInMemory(segmentBase, memoryBase + 21, colorWhite);
+
+	putInMemory(segmentBase, memoryBase + 22, '!');
+	putInMemory(segmentBase, memoryBase + 23, colorWhite);
+}
+
+void clearScreen()
+{
+	int i = memoryBase;
+
+	/*
+	for (i=memoryBase; i+=2; i<screenCharCount*2)
+	{
+		putInMemory(segmentBase, i, 'A');
+		putInMemory(segmentBase, i+1, colorWhite);
+	}
+	*/
+
+
+
+	/*
+	putInMemory(0xB000, 0x8140, 'A');
+	putInMemory(0xB000, 0x8141, 0x7);
+	*/
 	return;
 }
